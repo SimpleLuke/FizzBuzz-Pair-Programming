3adf5ae9d0fadbce342ec9511ca4b1da99d7cace
Luke Lai <lukelai.dev@gmail.com>
2023-01-10 10:51:16 +0000

    Make the first fizzbuzz test and define a fizzbuzz method to meet the minimum expectations.
    Co-authored-by: Luke <89473016+SimpleLuke@users.noreply.github.com>
    Co-authored-by: Abi <119049584+Ajmakovsky@users.noreply.github.com>"

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+# The program can be passed a number.
+# When passed a number that is a multiple of 3, the program returns the message 'Fizz'.
+# When passed a number that is a multiple of 5, the program returns the message 'Buzz'.
+# When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and returns the message 'FizzBuzz'.
+# In all other cases, the program simply returns the given number.
+
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed 3' do
+        expect(fizzbuzz(3)).to eq 'fizz'
+    end    
+end
